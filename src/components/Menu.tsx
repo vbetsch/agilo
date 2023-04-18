3774fdfd0f9d2b7bc5d665fce438445cff52c30f
vbetsch <vic.betsch@gmail.com>
2023-04-18 13:01:03 +0200

    ✨ Menu component

---
+++ src/components/Menu.tsx
@@ -0,0 +1,9 @@
+export function Menu() {
+  return (
+    <aside className="menu">
+        <div className="menu-content">
+            <img className="menu-content-logo" src="/img/logo.png" alt="logo" />
+        </div>
+    </aside>
+  );
+}
