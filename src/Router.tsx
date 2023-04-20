01cf2c82de6c21522db98a6e310ec574a272ac49
vbetsch <vic.betsch@gmail.com>
2023-04-20 23:56:48 +0200

    :sparkles: Default route

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,20 @@ import {createBrowserRouter} from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
 import LoginPage from "./pages/LoginPage";
 
+/*const userLoader = async () => {
+    const user = await getUser();
+    if (!user) {
+        return redirect("/login");
+    }
+    return null;
+};*/
+
 export const Router = createBrowserRouter([
+    {
+        path: "*",
+        element: <LoginPage />,
+        // loader: userLoader
+    },
     {
         path: "/login",
         element: <LoginPage />
