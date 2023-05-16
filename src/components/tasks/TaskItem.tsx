05219c305381e9b0cf1f1b087db214012e5f3cc5
vbetsch <vic.betsch@gmail.com>
2023-05-16 14:41:52 +0200

    :fire: Clean code

--- src/components/tasks/TaskItem.tsx
+++ src/components/tasks/TaskItem.tsx
@@ -24,7 +24,7 @@ export function TaskItem({task}: TaskItemProperties) {
             await findUsers(assignees, dispatch);
             // console.log(state.assignees)
         } catch (e) {
-            console.error(e)
+            // console.error(e)
         }
     }
 
